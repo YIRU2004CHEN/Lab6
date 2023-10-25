297cc6148e03f7390b6f7cb651d0a051e71f80c5
YIRU2004CHEN <149011290+YIRU2004CHEN@users.noreply.github.com>
2023-10-25 16:36:02 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+print('Menu')
+print('-------------')
+print('1. Encode')
+print('2. Decode')
+print('3. Quit')
+
+def encode_password(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+program = True
+
+if program:
+
+    option = input('Please enter an option: ')
+
+    if option == '1':
+        password = input('Please enter your password to encode: ')
+        encoded_password = encode_password(password)
+        print('Your password has been encoded and stored!')
+    elif option == '2':
+        print('000')
+
+    elif option == '3':
+
+        program = False
+
