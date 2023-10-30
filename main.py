aa8684ec0a402983607b991cce8e0cf8e9a6dba1
Yiru Chen <chenyiru@ufl.edu>
2023-10-30 14:16:25 -0400

    Yiru's Part 1

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-print('Menu')
-print('-------------')
-print('1. Encode')
-print('2. Decode')
-print('3. Quit')
-
+# Yiru Chen
 def encode_password(password):
+    # Initialize an empty string to store the encoded password.
     encoded_password = ""
+    # Iterate through each digit in the input password.
     for digit in password:
+        # Apply the encoding logic: shift the digit by 3 positions in a circular manner.
         encoded_digit = str((int(digit) + 3) % 10)
+        # Append the encoded digit to the result.
         encoded_password += encoded_digit
+    # Return the encoded password.
     return encoded_password
+
 
 program = True
 
 if program:
+
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
 
     option = input('Please enter an option: ')
 
