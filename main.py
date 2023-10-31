3a66448b72da06023172667ccdaa219fb6823db1
Yiru Chen <chenyiru@ufl.edu>
2023-10-31 11:07:42 -0400

    Yiru's Part 1

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ print('1. Encode')
 print('2. Decode')
 print('3. Quit')
 
-def encode_password(password):
-    encoded_password = ""
-    for digit in password:
-        encoded_digit = str((int(digit) + 3) % 10)
-        encoded_password += encoded_digit
-    return encoded_password
+
 
 program = True
+
+# Yiru Chen
+def encode_password(password):
+    # Initialize an empty string to store the encoded password.
+    encoded_password = ""
+    # Iterate through each digit in the input password.
+    for digit in password:
+        # Apply the encoding logic: shift the digit by 3 positions in a circular manner.
+        encoded_digit = str((int(digit) + 3) % 10)
+        # Append the encoded digit to the result.
+        encoded_password += encoded_digit
+    # Return the encoded password.
+    return encoded_password
 
 #Jess Barnes
 def decoding_password(password):
